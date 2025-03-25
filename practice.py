bc2feb1711c288910600500fb2bc3f766e93bd89
Saurabh Kamboj <Saurabhkamboj576@gmail.com>
2025-03-25 01:05:14 -0400

    Your commit message

--- practice.py
+++
@@ -1,4 +0,0 @@
-list_students = ["Sauabh", "Aman", "Abhishek","Tarun","Sumit"]
-for i in list_students():
-    if list_students[i].lower() =='s%'.lower():
-        print(f"Welcome {list_students[i]}")
